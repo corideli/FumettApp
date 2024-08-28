501f8faa48d77ca8cd4b2d55dea5c43a930649d6
corideli <corideli2003@gmail.com>
2024-08-28 17:26:33 +0200

    aggiunta funzione logout

--- app/src/main/java/it/insubria/fumettapp/MainActivity.kt
+++ app/src/main/java/it/insubria/fumettapp/MainActivity.kt
@@ -16,16 +16,31 @@ import it.insubria.fumettapp.DatabaseHelper.Companion.COLUMN_STATO
 import it.insubria.fumettapp.DatabaseHelper.Companion.COLUMN_COLLANA
 import it.insubria.fumettapp.DatabaseHelper.Companion.COLUMN_NUMERO_PAGINE
 
+import com.google.firebase.auth.FirebaseAuth
+import it.insubria.fumettapp.R
+import it.insubria.fumettapp.accesso.LoginActivity
+import it.insubria.fumettapp.databinding.ActivityMainBinding
+
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var firebaseAuth: FirebaseAuth
 
     companion object {
         const val REQUEST_CODE_CREATE_FILE = 1
     }
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        binding.btnLogout.setOnClickListener {
+            logout()
+        }
 
         val dbHelper = DatabaseHelper(this)
 
@@ -130,5 +145,13 @@ class MainActivity : AppCompatActivity() {
             cursor.close()
             db.close()
         }
-}
+    }
+
+    private fun logout() {
+        firebaseAuth.signOut() // Effettua il logout da Firebase
+        val intent = Intent(this, LoginActivity::class.java)
+        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK // Cancella lo stack di attività
+        startActivity(intent)
+        finish() // Chiude l'attuale attività
+    }
 }
